988ed162ddb08fe5c5970d424ed2b81e362408d2
Sightburner <kim.wahlman@gmail.com>
2016-01-02 04:30:07 +0100

    Merge Sort
    Added merge sort

--- SortingAlgorithms.h
+++ SortingAlgorithms.h
@@ -8,9 +8,51 @@ class Sorting
     std::vector<int> mSortedVector;
     int *mSortedArray, mSize;
     std::vector<int> GetVector() const { return mSortedVector; }
+
+	inline void Merge(int *arr, int l, int m, int r) {
+		int n1 = m - l + 1,
+			n2 = r - m;
+		unsigned int i = 0, j = 0, k = 0;
+
+		int* L = new int[n1];
+		int* R = new int[n2]; // Left and Right array.
+
+		for (i = 0; i < n1; i++)
+			L[i] = arr[l + i];
+		for (j = 0; j < n2; j++)
+			R[j] = arr[m + 1 + j];
+		i = 0;
+		j = 0;
+		k = l;
+
+		while (i < n1 && j < n2) {
+			if (L[i] < R[j]) {
+				arr[k] = L[i];
+				i++;
+			}
+			else {
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1) {
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+
+		while (j < n2) {
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+
     public:
         Sorting() : mSize(0) { }
-        ~Sorting() { delete[] mSortedArray; }
+		~Sorting() { }//delete[] mSortedArray; }
 
         // Sort in Ascending order
         inline void BubbleArrayAscend(int *a, int _size) {
@@ -109,6 +151,18 @@ class Sorting
 				unsigned int temp = arr[indexOfLargest];
 				arr[indexOfLargest] = arr[last];
 				arr[last] = temp;
+			}
+			
+		}
+
+		inline void MergeSort(int *arr, int l, int r, int size) {
+			mSize = size;
+			mSortedArray = new int[size];
+			if (l < r) {
+				int m = l + (r - l) / 2;
+				MergeSort(arr, l, m, size);
+				MergeSort(arr, m + 1, r, size);
+				Merge(arr, l, m, r);
 			}
 			mSortedArray = arr;
 		}
--- main.cpp
+++ main.cpp
@@ -1,34 +1,12 @@
 #include "SortingAlgorithms.h"
-#include <iostream>
 
 int main()
 {
-    Sorting s;
+	Sorting s;
+	int x[10] = { 0, 9, 2, 4, 7, 5, 6, 1, 3, 8 };
+	s.MergeSort(x, 0, 9, 10);
 
-    int x[10] = { 0, 9, 2, 4, 7, 5, 6, 1, 3, 8 },
-        y[10] = { 0, 9, 2, 4, 7, 5, 6, 1, 3, 8 };
-
-    std::vector<int> z = { 0, 9, 2, 4, 7, 5, 6, 1, 3, 8 };
-
-    std::cout << "Unsorted array & vector to sort:\n";
-    for(int i = 0; i < 10; i++)
-        std::cout << x[i] << " ";
-    std::cout << "\n";
-    for(int i = 0; i < 10; i++)
-        std::cout << z[i] << " ";
-
-    std::cout << "\n\nBubbleSort (Array): \n";
-    s.BubbleArrayAscend(x, 10);
-    s.printSortedArray();
-    std::cout << "\n";
-    s.BubbleArrayDescend(x, 10);
-    s.printSortedArray();
-
-    std::cout << "\n\nInsertionSort: \nArray\n";
-    s.InsertionSortArray(y, 10);
-    s.printSortedArray();
-    std::cout << "\nVector\n";
-    s.InsertionSortVector(z);
-    s.printSortedVector();
-    return 0;
+	s.printSortedArray();
+	std::cin.get();
+	return 0;
 }
